f0320ebd8e09af6f6dcd88792040066c12202079
Selena Pham <selena40253@gmail.com>
2025-03-12 22:25:30 -0700

    Module 2: Work with Express

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-var aloha = 'Aloha Variables';
-console.log(aloha);
+var express = require('express');   // references the express package
+var app = express();    // creates express instance
+
+var port = 5000; // for cloud9 IDE: process.env.PORT;
+
+app.get('/', function(req, res) {
+    res.send('Aloha World!');
+});
+
+app.get('/routing', function(req, res) {
+    res.send('Aloha Routing!');
+});
+
+app.listen(port, function(err) {
+    console.log(`The server is running on port: http://localhost:` + port);
+});
